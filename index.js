7501b0107ee71909a19bef4a155ae5bdc0737fc0
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-12-07 20:10:54 -0300

    feat: initial resolution

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * 4. Crie uma função chamada multiplica que recebe dois parâmetros numéricos. Ela deverá
+ * exibir a multiplicação desses dois parâmetros no console.
+ */
+function multiplica(numero1, numero2) {
+  const resultado = numero1 * numero2;
+  console.log("Execício 4:", resultado);
+}
+
+multiplica(2, 2);
+
+/**
+ * 6. Você deve criar um loop usando o for, que exibe no console a sequência de números do 0
+ * ao 20.
+ */
+console.log("Execício 6:");
+for (var posicao = 0; posicao <= 20; posicao++) {
+  console.log(posicao);
+}
